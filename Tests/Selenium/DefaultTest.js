671079b82b21dda97e20ee2bb2c93d78667e3ec2
nehalist <github@nehalist.io>
2018-07-27 12:50:42 +0200

    Test against browserstack

--- Tests/Selenium/DefaultTest.js
+++ Tests/Selenium/DefaultTest.js
@@ -3,13 +3,23 @@ const {Builder, By, until} = require('selenium-webdriver');
 
 describe('default test', () => {
     const driver = new Builder()
-        .forBrowser('chrome')
+        .usingServer('http://hub-cloud.browserstack.com/wd/hub')
+        .withCapabilities({
+            'browserName' : 'Chrome',
+            'browser_version' : '62.0',
+            'os' : 'Windows',
+            'os_version' : '10',
+            'resolution' : '1920x1080',
+            'browserstack.user' : process.env.BS_USER,
+            'browserstack.key' : process.env.BS_KEY,
+            'browserstack.local' : 'true'
+        })
         .build();
 
     const url = 'http://localhost';
 
     it('should check the page title', async () => {
-        await driver.get(`${url}/account`);
+        await driver.get(`${url}`);
 
         const pageTitle = await driver.getTitle();
 
